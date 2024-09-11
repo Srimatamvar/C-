bdd66e4c97729d287e6a431fb549400d6ee8156b
Srimatamvar Singh <122569682+Srimatamvar@users.noreply.github.com>
2024-09-11 08:45:16 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace ssITP10CSharpPractical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Recursion : Print even or odd numbers in a given range: ");
+            Console.WriteLine("------------------------------------------------------");
+            Console.Write("Input the range to print starting from 1: ");
+
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"All even numbers from 1 to {n} are:\n");
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                    Console.Write($"{i}\t");
+
+            }
+            Console.Write($"\nAll odd numbers from 1 to {n} are: \n");
+            for (int i = 1; i <= n; i++) {
+                
+                if (i % 2 != 0)
+                    Console.Write($"{i}\t");
+
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
